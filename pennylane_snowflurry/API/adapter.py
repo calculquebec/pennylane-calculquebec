8e221abd30fe83a926f3f28e6e6a4354c5a21001
Felix Boucher <boucherlfg@gmail.com>
2024-12-02 09:48:41 -0500

    update doc + put slowly changing nw ops in cache

--- pennylane_snowflurry/API/adapter.py
+++ pennylane_snowflurry/API/adapter.py
@@ -2,9 +2,14 @@ from pennylane_snowflurry.utility.api import ApiUtility, routes, keys
 import requests
 import json
 from pennylane_snowflurry.API.client import ApiClient
-
+from datetime import datetime, timedelta
 
 class ApiAdapter(object):
+    _qubits_and_couplers = None
+    _machine = None
+    _benchmark = None
+    _last_update = None
+    
     """
     a wrapper around Thunderhead. Provide a host, user, access token and realm, and you can :
     - create jobs with circuit dict, circuit name, project id, machine name and shots count
@@ -39,32 +44,45 @@ class ApiAdapter(object):
         """
         get the id of a machine by using the machine's name stored in the client
         """
-        route = ApiAdapter.instance().client.host + routes.machines + routes.machineName + "=" + ApiAdapter.instance().client.machine_name
-        return requests.get(route, headers=ApiAdapter.instance().headers)
+        
+        # put machine in cache
+        if ApiAdapter._machine is None:
+            route = ApiAdapter.instance().client.host + routes.machines + routes.machineName + "=" + ApiAdapter.instance().client.machine_name
+            res = requests.get(route, headers=ApiAdapter.instance().headers)
+            if res != 200:
+                return None
+            ApiAdapter._machine = json.loads(res.text)
+            
+        return ApiAdapter._machine
     
     @staticmethod
     def get_qubits_and_couplers() -> dict[str, any] | None:
         """
         get qubits and couplers informations from latest benchmark
         """
-        res = ApiAdapter.instance().get_benchmark()
-        if res.status_code != 200:
-            return None
-        return json.loads(res.text)[keys.resultsPerDevice]
+        
+        benchmark = ApiAdapter.instance().get_benchmark()
+        return benchmark[keys.resultsPerDevice]
 
     @staticmethod
     def get_benchmark():
         """
         get latest benchmark for a given machine (machine name stored in client)
         """
-        res = ApiAdapter.instance().get_machine_id_by_name()
-        if res.status_code != 200:
-            return None
-        result = json.loads(res.text)
-        machine_id = result[keys.items][0][keys.id]
-    
-        route = ApiAdapter.instance().client.host + routes.machines + "/" + machine_id + routes.benchmarking
-        return requests.get(route, headers=ApiAdapter.instance().headers)
+
+        # put benchmark in cache
+        if ApiAdapter._benchmark is None or ApiAdapter._last_update - datetime.now() > timedelta(hours=24):
+            machine = ApiAdapter.instance().get_machine_id_by_name()
+            machine_id = machine[keys.items][0][keys.id]
+
+            route = ApiAdapter.instance().client.host + routes.machines + "/" + machine_id + routes.benchmarking
+            res = requests.get(route, headers=ApiAdapter.instance().headers)
+            if res != 200:
+                return None
+            ApiAdapter._benchmark = json.loads(res.text)
+            ApiAdapter._last_update = datetime.now()
+            
+        return ApiAdapter._benchmark
     
     @staticmethod
     def create_job(circuit : dict[str, any], 
