02144cd7ab6a1c27a4104730ee7e7c7488599000
Felix Boucher <boucherlfg@gmail.com>
2024-12-04 13:45:37 -0500

    update test file

--- pennylane_snowflurry/test.py
+++ pennylane_snowflurry/test.py
@@ -3,21 +3,35 @@ import pennylane as qml
 from pennylane_snowflurry.monarq_device import MonarqDevice
 from pennylane_snowflurry.utility.test_device import TestDevice
 from pennylane_snowflurry.API.client import MonarqClient
-from pennylane_snowflurry.processing.config import NoPlaceNoRouteConfig
-from dotenv import dotenv_values
+from pennylane_snowflurry.processing.config import NoPlaceNoRouteConfig, MonarqDefaultConfig
+from pennylane_snowflurry.processing.steps import ReadoutErrorMitigation, ISMAGS
+
 from pennylane.devices import DefaultQubit
+from pennylane_snowflurry.API.adapter import ApiAdapter
+from pennylane_snowflurry.utility.graph import calculate_cost, machine_graph, keys
+from pennylane_snowflurry.utility.plotting import graph
+import matplotlib.pyplot as plt
+from dotenv import dotenv_values
 conf = dotenv_values(".env")
 
 client = MonarqClient(conf["HOST"], conf["USER"], conf["ACCESS_TOKEN"], conf["PROJECT_NAME"])
 
-transpiler_config = NoPlaceNoRouteConfig()
-# transpiler_config.steps.append(ReadoutErrorMitigation())
-dev_transpiler = MonarqDevice(shots=1000, client = client)
-@qml.qnode(dev_transpiler)
-def circuit():
-    qml.RX(np.pi/2, 0)
-    return qml.counts()
+transpiler_config = MonarqDefaultConfig()
+dev_transpiler = MonarqDevice(shots=1000, client = client, processing_config=transpiler_config)
 
-results = circuit()
-print(results)
-exit()
+for i in range(1, 8):
+    
+    @qml.qnode(dev_transpiler)
+    def circuit():
+        qml.Hadamard(0)
+        for j in range(i):
+            qml.CNOT([0, j+1])
+        return qml.counts()
+
+    results = circuit()
+    print(results)
+    plt.bar([v for v in results.keys()], [v for v in results.values()])
+    plt.show()
+            
+# expliquer la différence entre les portes qml et custom
+# exit()
