0582ceed0040f662051d4c4cf9bf0ce6437ba83a
Scirelgar <81189385+Scirelgar@users.noreply.github.com>
2024-12-11 08:43:03 -0500

    converted test_pennylaneConverter.py

--- tests/test_pennylaneConverter.py
+++ tests/test_pennylaneConverter.py
@@ -3,15 +3,11 @@ import pennylane as qml
 import numpy as np
 import logging
 from pennylane.tape import QuantumTape
-import unittest
 
-class Test_PennylaneConverterClass(unittest.TestCase):
+class Test_PennylaneConverterClass:
+
     def test_quantumTape(self):
         ops = [qml.BasisState(np.array([1,1]), wires=(0,"a"))]
         quantumTape = QuantumTape(ops, [qml.expval(qml.PauliZ(0))])
         converter = pennylane_snowflurry.PennylaneConverter(quantumTape)
-        self.assertIsInstance(converter.pennylane_circuit, QuantumTape)
-    
-
-if __name__ == '__main__':
-    unittest.main()
+        assert isinstance(converter.pennylane_circuit, QuantumTape)
