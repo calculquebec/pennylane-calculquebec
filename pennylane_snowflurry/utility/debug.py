5d960d8788bb097701c5e7c4f92ab2637eed6fc6
Felix Boucher <boucherlfg@gmail.com>
2024-12-11 11:21:44 -0500

    python310

--- pennylane_snowflurry/utility/debug.py
+++ pennylane_snowflurry/utility/debug.py
@@ -38,108 +38,17 @@ def to_qasm(tape : QuantumTape) -> str:
         "S" : "s", "Adjoint(S)" : "sdg", "SX" : "sx", "Adjoint(SX)" : "sxdg", "T" : "t", "Adjoint(T)" : "tdg", 
         "CNOT" : "cx", "CY" : "cy", "CZ" : "cz", "SWAP" : "swap",
         "Z90" : "s", "ZM90" : "sdg", "X90" : "sx", "XM90" : "sxdg", "Y90" : "ry(pi/2)", "YM90" : "ry(3*pi/2)",
-        "TDagger" : "tdg"
+        "TDagger" : "tdg", "CRY" : "cry"
     }
-    return "\n".join([eq[op.name] \
-        + (f"({op.parameters[0]}) " if len(op.parameters) > 0 else " ") \
-        + ", ".join([f"q[{w}]" for w in op.wires]) \
-        + ";" for op in tape.operations])
-
-class SnowflurryUtility:
-    """
-    methods for interacting between pennylane and snowflurry
-    """
-    def __init__(self, tape : QuantumTape, host, user, access_token, realm):
-        self.converter = PennylaneConverter(tape, 
-                                       wires=24,
-                                       host=host, 
-                                       user=user, 
-                                       access_token=access_token, 
-                                       realm=realm, 
-                                       project_id="default")
-    
-    def gate_count(self, sf_circuit = None) -> int:
-        """method for returning the number of gates in a snowflurry circuit
-
-        Args:
-            sf_circuit (optional): a snowflurry circuit. circuit will be set to the one in the Snowflurry package if none is provided.
-
-        Returns:
-            int: the number of gates in the circuit
-        """
-        if sf_circuit is None: sf_circuit = Snowflurry.sf_circuit
-        return len(sf_circuit.instructions)
-
-    def to_pennylane(self, sf_circuit = None, measurement_process = qml.probs) -> QuantumTape:
-        """
-        turns a snowflurry circuit into a Pennylane QuantumTape
-        """
-        if sf_circuit is None: sf_circuit = Snowflurry.sf_circuit
-
-        new_operations = []
-        measurements = {}
-
-        assoc = {"Snowflurry.Z90" : custom.Z90, "Snowflurry.ZM90" : custom.ZM90, 
-                 "Snowflurry.X90" : custom.X90, "Snowflurry.XM90" : custom.XM90, 
-                 "Snowflurry.Y90" : custom.Y90, "Snowflurry.YM90" : custom.YM90,
-                 "Snowflurry.SigmaX" : qml.PauliX, "Snowflurry.SigmaY" : qml.PauliY, "Snowflurry.SigmaZ" : qml.PauliZ, 
-                 "Snowflurry.Pi8" : qml.T, "Snowflurry.Pi8Dagger" : custom.TDagger, "Snowflurry.ControlZ" : qml.CZ}
-
-        
-        for i in sf_circuit.instructions:
-            if "Readout" in str(i):
-                qubit = i.connected_qubit
-                bit = i.destination_bit
-                measurements[int(bit) - 1] = int(qubit) - 1
-                continue
-
-            symbol_and_param = i.symbol
-            symbol_and_param = str(symbol_and_param).split("(")
-            
-            symbol = symbol_and_param[0]
-            param = symbol_and_param[1].split(")")[0] if len(symbol_and_param) > 1 else None
-            connected_qubits = i.connected_qubits
-
-            op = partial(qml.PhaseShift, float(param)) if "PhaseShift" in symbol else assoc[symbol]
-            new_operations += [op([int(w) - 1 for w in connected_qubits])]
-
-        measurements = list(sorted(measurements.values(), key=lambda v: [m for m in measurements.items() if m[1] == v][0]))
-        measurements = [measurement_process(wires=measurements)]
-        return QuantumTape(ops = new_operations, measurements = measurements, shots = 1000)
-
-    def to_qasm(self, sf_circuit = None) -> str:
-        if sf_circuit is None: sf_circuit = Snowflurry.sf_circuit
-
-        result = []
-        
-        assoc = {"Snowflurry.Z90" : "s", "Snowflurry.ZM90" : "sdg", 
-                 "Snowflurry.X90" : "sx", "Snowflurry.XM90" : "sxdg", 
-                 "Snowflurry.Y90" : "ry(pi/2)", "Snowflurry.YM90" : "ry(3*pi/2)",
-                 "Snowflurry.SigmaX" : "x", "Snowflurry.SigmaY" : "y", "Snowflurry.SigmaZ" : "z", 
-                 "Snowflurry.Pi8" : "t", "Snowflurry.Pi8Dagger" : "tdg", "Snowflurry.ControlZ" : "cz"}
-
-        for i in sf_circuit.instructions:
-            if "Readout" in str(i):
-                continue
-            symbol_and_param = i.symbol
-            symbol_and_param = str(symbol_and_param).split("(")
-            symbol = symbol_and_param[0]
-            param = symbol_and_param[1].split(")")[0] if len(symbol_and_param) > 1 else None
-            connected_qubits = i.connected_qubits
-            connected_qubits = str(connected_qubits)
-            result.append(f"{assoc[symbol]}{"(" + param + ") " if param != "" else " "} {connected_qubits}")
-        return "\n".join(result)
-
-    def transpile(self):
-        """
-        Transpiles a snowflurry circuit using the snowflurry transpiler
-        """
-        sf_circuit = self.converter.convert_circuit(self.converter.pennylane_circuit)
-        [[self.converter.apply_single_readout(w) for w in mp.wires] for mp in self.converter.pennylane_circuit.measurements]
-
-        qpu = Snowflurry.AnyonYamaskaQPU(Snowflurry.currentClient, Snowflurry.seval("project_id"))
-        sf_circuit = Snowflurry.transpile(Snowflurry.get_transpiler(qpu), sf_circuit)
-        Snowflurry.sf_circuit = sf_circuit
+    total_string = ""
+    for op in tape.operations:
+        string = eq[op.name]
+        string += str(op.parameters[0]) if len(op.parameters) > 0 else ""
+        string += " "
+        string += ", ".join([f"q[{w}]" for w in op.wires])
+        string += ";"
+        total_string += string + "\n"
+    return total_string
 
 def arbitrary_circuit(tape : QuantumTape, measurement = qml.counts):
     """
