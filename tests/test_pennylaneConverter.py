7ee44ae85f3fbb3efc102d6af3f063705154f6a7
Scirelgar <81189385+Scirelgar@users.noreply.github.com>
2024-12-12 08:14:11 -0500

    rebase refactor/isolate-sf-device onto feature/thunderhead_plugin

--- tests/test_pennylaneConverter.py
+++ tests/test_pennylaneConverter.py
@@ -4,9 +4,9 @@ import numpy as np
 import logging
 from pennylane.tape import QuantumTape
 
-class Test_PennylaneConverterClass:
+class TestPennylaneConverterClass:
 
-    def test_quantumTape(self):
+    def test_quantumtape(self):
         ops = [qml.BasisState(np.array([1,1]), wires=(0,"a"))]
         quantumTape = QuantumTape(ops, [qml.expval(qml.PauliZ(0))])
         converter = pennylane_snowflurry.PennylaneConverter(quantumTape)
