08ef705976a27ab17e01876d7aae1c91814d7954
boucherlfg <boucherlfg@gmail.com>
2025-02-03 13:25:22 -0500

    make snowflurry optional

--- pennylane_calculquebec/__init__.py
+++ pennylane_calculquebec/__init__.py
@@ -1,5 +1,10 @@
 """this is the top level module for the Pennylane Snowflurry plugin. It is used for communicating with MonarQ.
 """
+import importlib.util
 
-from .snowflurry_device import SnowflurryQubitDevice
+if importlib.util.find_spec("juliacall") is not None:
+    from .snowflurry_device import SnowflurryQubitDevice
+else:
+    print("won't import snowflurry.qubit")
+
 from .monarq_device import MonarqDevice
