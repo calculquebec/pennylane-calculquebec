19c80b2c66434f0727a8a6945e90ebd92d87ebb1
Bergui123 <bergeronx@gmail.com>
2025-06-07 11:06:51 -0400

    Added the file creation in the working directory

--- pennylane_calculquebec/logger.py
+++ pennylane_calculquebec/logger.py
@@ -1,9 +1,14 @@
 import logging
+import os
+
+# Get the root directory (one level up from this file's directory)
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+LOG_PATH = os.path.join(ROOT_DIR, "pennylane_calculquebec.log")
 
 logger = logging.getLogger("pennylane_calculquebec")
 logger.setLevel(logging.INFO)
 
-handler = logging.FileHandler("pennylane_calculquebec.log", mode="a", encoding="utf-8")
+handler = logging.FileHandler(LOG_PATH, mode="a", encoding="utf-8")
 formatter = logging.Formatter(
     'The plugin version is 0.6.1 | %(asctime)s | Incident: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
