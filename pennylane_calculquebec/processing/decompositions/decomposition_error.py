f2740a6bdaa439d883c404f0f955be16e1398d15
Xavier <xavier@kali>
2025-07-29 15:19:53 -0400

    Removed the unused error classes

--- pennylane_calculquebec/processing/decompositions/decomposition_error.py
+++
@@ -1,8 +0,0 @@
-from .. import ProcessingError
-class DecompositionError(ProcessingError):
-    """Error related to decomposition."""
-    def __init__(self, message: str):
-        predefined = "Decomposition Error/"
-        full_message = f"{predefined}{message}"
-        super().__init__(full_message)
-        self.message = full_message
