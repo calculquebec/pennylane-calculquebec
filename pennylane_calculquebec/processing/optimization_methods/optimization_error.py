ee22f6856ad7533df0ba8105b84dbdbf5fc6e8d2
Xavier <xavier@kali>
2025-07-29 15:19:53 -0400

    Removed the unused error classes

--- pennylane_calculquebec/processing/optimization_methods/optimization_error.py
+++
@@ -1,8 +0,0 @@
-from .. import ProcessingError
-class OptimizationError(ProcessingError):
-    """Error related to optimization."""
-    def __init__(self, message: str):
-        predefined = "Optimization Error/"
-        full_message = f"{predefined}{message}"
-        super().__init__(full_message)
-        self.message = full_message 
